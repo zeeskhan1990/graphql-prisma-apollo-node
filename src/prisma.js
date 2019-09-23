a3ccceb1ffe8569bd5e798958a188857f3642dbd
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-09-23 18:20:48 +0530

    prisma env

--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,7 @@ import {fragmentReplacements} from './resolvers/index'
 const prisma = new Prisma({
     typeDefs:'src/generated/prisma.graphql',
     endpoint:process.env.PRISMA_ENDPOINT,
-    secret:'zeeshankhan',
+    secret:process.env.PRISMA_SECRET,
     fragmentReplacements
 })
 
